13e0d42b0e0a42b2fe1885429f9d5df5b183641d
michaelsam <137851066+msam4@users.noreply.github.com>
2024-07-31 16:35:43 +0900

    Create add and show all method for simple note app

---
+++ manager.rb
@@ -0,0 +1,21 @@
+require './database'
+
+class Manager
+  def initialize
+    @database = Database.new
+  end
+
+  def store(note)
+    @database.store(note)
+  end
+
+  def show_all
+    entries = @database.get_all
+    puts "No Entries Found" if entries.empty?
+    entries.each_with_index do |item, index|
+      puts (index + 1).to_s.center(50, "-")
+      puts item
+      puts "".center(50, "-")
+    end
+  end
+end
---
+++ note.rb
@@ -0,0 +1,15 @@
+require 'securerandom'
+
+class Notes
+  attr_reader :id
+
+  def initialize(text)
+    @id = SecureRandom.uuid
+    @text = text
+    @date = Time.now
+  end
+
+  def to_s
+    "Date: #{@date.strftime("%Y-%m-%d %H:%M:%S")} \nNote: #@text"
+  end
+end
